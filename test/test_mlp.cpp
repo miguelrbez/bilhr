d5de540df5d39190b0d5f664e4f709de293cc99a
Martin Patz <mailto@martin-patz.de>
2016-05-29 19:16:55 +0200

    test file

--- test/test_mlp.cpp
+++ test/test_mlp.cpp
@@ -1,61 +1,43 @@
 #include "../src/mlp.cpp"
 #include <iostream>
+#include <vector>
 
 
 int main(int argc, char *argv[])
 {
-//    MLP mlp (3, 6, 2, 0.6, 0.5, 0.4);
-//	std::cout << "Hello World.\n";
-//	std::vector<double> sample_input = {0.1, -0.4, 5.1};
-//	std::vector<double> sample_output = {0.2, -0.81, 8};
-//	mlp.add_sample(sample_input, sample_output);
-
     // task 2 XOR
     MLP mlp(2, 2, 1, 0.05, 0.05, 0.00002);
-    mlp.set_max_iterations(20000);
-    
+    mlp.set_max_iterations(200000);
     // input sample
-    vector<double> inputSample;
-    vector<double> outputSample;
+    std::vector< std::vector<double> > input_samples;
+    std::vector< std::vector<double> > output_samples;
 
     // XOR - first line
-    inputSample.push_back(0.0001);
-    inputSample.push_back(0.0001);
-    outputSample.push_back(0.0001);
-    mlp.add_sample(inputSample, outputSample);
-
-    // clear vector
-    inputSample.clear();
-    outputSample.clear();
+    input_samples.push_back({0.0001, 0.0001});
+    output_samples.push_back({0.0001});
+    mlp.add_sample(input_samples.back(), output_samples.back());
 
     // XOR - second line
-    inputSample.push_back(0.9999);
-    inputSample.push_back(0.0001);
-    outputSample.push_back(0.9999);
-    mlp.add_sample(inputSample, outputSample);
-
-    // clear vector
-    inputSample.clear();
-    outputSample.clear();
+    input_samples.push_back({0.9999, 0.0001});
+    output_samples.push_back({0.9999});
+    mlp.add_sample(input_samples.back(), output_samples.back());
 
     // XOR - third line
-    inputSample.push_back(0.0001);
-    inputSample.push_back(0.9999);
-    outputSample.push_back(0.9999);
-    mlp.add_sample(inputSample, outputSample);
-
-    // clear vector
-    inputSample.clear();
-    outputSample.clear();
+    input_samples.push_back({0.0001, 0.9999});
+    output_samples.push_back({0.9999});
+    mlp.add_sample(input_samples.back(), output_samples.back());
 
     // XOR - forth line
-    inputSample.push_back(0.9999);
-    inputSample.push_back(0.9999);
-    outputSample.push_back(0.0001);
-    mlp.add_sample(inputSample, outputSample);
+    input_samples.push_back({0.9999, 0.9999});
+    output_samples.push_back({0.0001});
+    mlp.add_sample(input_samples.back(), output_samples.back());
 
     mlp.train();
 
-    mlp.save();
-    mlp.load();
+    for (int i = 0; i < input_samples.size(); i++)
+        printf("Input: %1.4f, %1.4f | Output = %1.4f | Expected Output = %1.4f\n", 
+            input_samples.at(i)[0], input_samples.at(i)[1],
+            mlp.evaluate(input_samples.at(i))[0], output_samples.at(i)[0]);
+
+    mlp.save("~/Desktop/bilhr_weights");
 }
