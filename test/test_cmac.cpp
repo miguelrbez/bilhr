338eed75563d623be129648bb1924fae7c7fb16d
BILHR Group E <bilhr@martin-patz.de>
2016-06-06 10:55:56 +0200

    reorganized folder structure

--- test/test_cmac.cpp
+++ test/test_cmac.cpp
@@ -3,7 +3,8 @@
 
 int main(int argc, char const *argv[])
 {
-	CMAC cmac_t = CMAC(2, 3, 10, 0.00002);
+	CMAC cmac_case_a = CMAC(2, 3, 50, 0.002);
+	CMAC cmac_case_b = CMAC(2, 3, 50, 0.002);
 	printf("Loading CMAC successfull!\n");
 	return 0;
 }
