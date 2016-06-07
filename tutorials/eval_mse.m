6262242b08219b464f759dc2accebab81f048e62
Martin Patz <mailto@martin-patz.de>
2016-06-07 13:56:04 +0200

    compared training performance between MSE and CMAC

--- tutorials/eval_mse.m
+++ tutorials/eval_mse.m
@@ -3,13 +3,26 @@
 % iteration and mse.
 
 %% Pre-define parameters
-figure_name = 'Tutorial 4, Task 2, Case B, Receptive Field Size 5';
+figure_name = 'Tutorial 4, Task 4, Training comparison between MLP and CMAC';
 font_size = 20;
 
 
 %% Create figure and plot
 f = figure('Name', figure_name);
-plot(iteration, mse);
+hold on
+plot(iteration_2(1:150), mse_2(1:150));
+plot(iteration_3(1:150), mse_3(1:150));
+plot(iteration_a_3, mse_a_3);
+plot(iteration_a_5, mse_a_5);
+plot(iteration_b_3, mse_b_3);
+plot(iteration_b_5, mse_b_5);
+hold off
+legend(	'Tutorial 3 MLP, 34 TS, Task 2', ...
+		'Tutorial 3 MLP, 34 TS, Task 3', ...
+		'Tutorial 4 CMAC, 75 TS, 3x3 PF', ...
+		'Tutorial 4 CMAC, 75 TS, 5x5 PF', ...
+		'Tutorial 4 CMAC, 150 TS, 3x3 PF', ...
+		'Tutorial 4 CMAC, 150 TS, 5x5 PF');
 xlabel('Iteration', 'FontSize', font_size);
 ylabel('MSE', 'FontSize', font_size);
 title(['Plot the Mean Square Error (MSE) over the Iterations for ', ...
