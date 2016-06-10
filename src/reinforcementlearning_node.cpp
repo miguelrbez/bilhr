acf01bba72f227088942d8a601f47b8fe5b318fe
BILHR Group E <bilhr@martin-patz.de>
2016-06-10 19:11:06 +0200

    rl with main function launchfile which starts rl_node

--- src/reinforcementlearning_node.cpp
+++ src/reinforcementlearning_node.cpp
@@ -5,4 +5,39 @@
   * - Miguel Rodriguez
   * - Martin Patz
   * - Adam Zylka
+  *
+  * HOW TO START
+roslaunch nao_bringup nao_full_py.launch nao_ip:=10.152.246.242 roscore_ip:=10.152.246.128
+roslaunch nao_apps tacticle.launch nao_ip:=10.152.246.242 roscore_ip:=10.152.246.128
+roslaunch bl_group_e start.launch
 */
+
+// ros includes
+#include <ros/ros.h>
+#include <sensor_msgs/image_encodings.h>
+#include <sensor_msgs/Image.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/time_synchronizer.h>
+#include <std_msgs/Float32.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int32.h>
+#include "std_msgs/String.h"
+
+// cpp standard includes
+#include <iostream>
+
+using namespace std;
+using namespace sensor_msgs;
+using namespace message_filters;
+
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "reinforcementlearning_node");
+    ros::NodeHandle rl_node_nh;
+    cout << "starting reinforcementlearning_node" << endl;
+
+    ros::spin();
+
+    return 0;
+}
