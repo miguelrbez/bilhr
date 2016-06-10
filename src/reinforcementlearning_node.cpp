3132bce8b98b271de8fe6af138782e25d7432e70
BILHR Group E <bilhr@martin-patz.de>
2016-06-10 19:23:23 +0200

    added bumper_cb, tactile_cb, stiffness-advertise (but no function)

--- src/reinforcementlearning_node.cpp
+++ src/reinforcementlearning_node.cpp
@@ -26,16 +26,91 @@ roslaunch bl_group_e start.launch
 // cpp standard includes
 #include <iostream>
 
+// robot config
+#include "robot_config.h"
+
 using namespace std;
 using namespace sensor_msgs;
 using namespace message_filters;
 
+/***************************
+* LOCAL DEFINITIONS
+***************************/
+// subscribers to tactile and touch sensors
+ros::Subscriber tactile_sub;
+ros::Subscriber bumper_sub;
 
+// joint stiffnesses
+ros::Publisher stiffness_pub;
+
+/***************************
+* CALLBACK - FUNCTIONS
+***************************/
+// callback function for tactile buttons (TBs) on the head
+void tactileCB(const robot_specific_msgs::TactileTouch::ConstPtr& __tactile_touch)
+{
+    // check TB 3 (rear)
+    if (((int)__tactile_touch->button == 3) && ((int)__tactile_touch->state == 1))
+    {
+        cout << "TB " << (int)__tactile_touch->button << " touched" << endl;
+    }
+
+    // check TB 2 (middle)
+    if (((int)__tactile_touch->button == 2) && ((int)__tactile_touch->state == 1))
+    {
+        cout << "TB " << (int)__tactile_touch->button << " touched" << endl;
+    }
+
+    // check TB 1 (front)
+    if (((int)__tactile_touch->button == 1) && ((int)__tactile_touch->state == 1))
+    {
+        cout << "TB " << (int)__tactile_touch->button << " touched" << endl;
+    }
+
+}
+
+
+// callback function for bumpers
+void bumperCB(const robot_specific_msgs::Bumper::ConstPtr& __bumper)
+{
+    static bool left_bumper_flag = false;
+    static bool right_bumper_flag = false;
+
+    // check left bumper
+    if (((int)__bumper->bumper == 1) && ((int)__bumper->state == 1))
+    {
+        left_bumper_flag = !left_bumper_flag;   // toggle flag
+        cout << "pressed left bumper\n";
+    }
+
+    // check right bumper
+    if (((int)__bumper->bumper == 0) && ((int)__bumper->state == 1))
+    {
+        right_bumper_flag = !right_bumper_flag;     // toggle flag
+        cout << "pressed right bumper\n";
+    }
+
+}
+
+
+/***************************
+* MAIN
+***************************/
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "reinforcementlearning_node");
     ros::NodeHandle rl_node_nh;
+
     cout << "starting reinforcementlearning_node" << endl;
+
+    // advertise joint stiffnesses
+    stiffness_pub = rl_node_nh.advertise<robot_specific_msgs::JointState>("joint_stiffness", 1);
+
+    // subscribe to tactile and touch sensors
+    tactile_sub = rl_node_nh.subscribe("tactile_touch", 1, tactileCB);
+    bumper_sub = rl_node_nh.subscribe("bumper", 1, bumperCB);
+
+
 
     ros::spin();
 
