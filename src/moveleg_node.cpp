0e195034a670488238a1dbacb884f0fa914a90f8
BILHR Group E <bilhr@martin-patz.de>
2016-06-10 22:25:02 +0200

    joint state publisher added to ml_node ml_node is still not finished also missing comunication between ml_node and rl_node

--- src/moveleg_node.cpp
+++ src/moveleg_node.cpp
@@ -27,8 +27,54 @@ using namespace message_filters;
 ros::Subscriber tactile_sub;
 ros::Subscriber bumper_sub;
 
-// joint stiffnesses
+// subscribe to joint states
+ros::Subscriber joint_state_sub;
+
+// publisher to robot joints for sending the target joint angles
+ros::Publisher target_joint_state_pub;
+
+// publish joint stiffnesses
 ros::Publisher stiffness_pub;
+
+// received motor state of the HEAD
+double motor_head_in[HEAD_DOF];
+
+// received motor state of the LEFT ARM
+double motor_l_arm_in[L_ARM_DOF];
+
+// received motor state of the RIGHT ARM
+double motor_r_arm_in[R_ARM_DOF];
+
+
+/***************************
+* LOCAL FUNCTIONS
+***************************/
+// send commanded joint positions of the HEAD
+void sendTargetJointStateHead(/* maybe a result as function argument */)
+{
+    double dummy[HEAD_DOF];  // dummy representing the comanded joint state
+    robot_specific_msgs::JointAnglesWithSpeed target_joint_state;
+
+    // specify the limb
+    target_joint_state.joint_names.clear();
+    target_joint_state.joint_names.push_back("Head");
+
+    // specifiy the angle
+    target_joint_state.joint_angles.clear();
+    for (int i=0; i<HEAD_DOF; i++)
+        target_joint_state.joint_angles.push_back(dummy[i] /* array containing result */);
+
+    // set speed
+    target_joint_state.speed = 0.2;
+
+    // set the mode of joint change
+    target_joint_state.relative = 0;
+
+    // send to robot
+    target_joint_state_pub.publish(target_joint_state);
+
+}
+
 
 /***************************
 * CALLBACK - FUNCTIONS
@@ -77,6 +123,155 @@ void bumperCB(const robot_specific_msgs::Bumper::ConstPtr& __bumper)
     }
 }
 
+// callback function for the head joints
+void jointStateCB(const robot_specific_msgs::JointState::ConstPtr& joint_state)
+{
+    // buffer for incoming message
+    std_msgs::Float32MultiArray buffer;
+
+    // index
+    int idx;
+
+
+    // extract the proprioceptive state of the HEAD
+    buffer.data.clear();
+    for (int i=0; i<ROBOT_DOF; i++)
+    {
+        if (joint_state->name[i] == "HeadYaw")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << ": " << joint_state->position[i] << endl;
+        }
+        if (joint_state->name[i] == "HeadPitch")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << ": " << joint_state->position[i] << endl;
+        }
+    }
+
+    // write data into array
+    idx = 0;
+    for(vector<float>::const_iterator iter = buffer.data.begin(); iter != buffer.data.end(); ++iter)
+    {
+        // store into temporary target motor state buffer
+        motor_head_in[idx] = *iter;
+        idx++;
+    }
+
+    // display data on terminal
+    // cout << "Head joints:  ";
+    // for (int i=0; i<HEAD_DOF; i++)
+    //     cout << motor_head_in[i] << " ";
+    // cout << endl;
+
+
+    // extract the proprioceptive state of the LEFT ARM
+    buffer.data.clear();
+    for (int i=0; i<ROBOT_DOF; i++)
+    {
+        if (joint_state->name[i] == "LShoulderPitch")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "LShoulderRoll")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "LElbowYaw")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "LElbowRoll")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "LWristYaw")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "LHand")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+
+    }
+
+    // write data into array
+    idx = 0;
+    for(vector<float>::const_iterator iter = buffer.data.begin(); iter != buffer.data.end(); ++iter)
+    {
+        // store into temporary target motor state buffer
+        motor_l_arm_in[idx] = *iter;
+        idx++;
+    }
+
+    // display data on terminal
+    // cout << "Left arm joints:  ";
+    // for (int i=0; i<L_ARM_DOF; i++)
+    //     cout << motor_l_arm_in[i] << " ";
+    // cout << endl;
+
+
+    // extract the proprioceptive state of the RIGHT ARM
+    buffer.data.clear();
+    for (int i=0; i<ROBOT_DOF; i++)
+    {
+        if (joint_state->name[i] == "RShoulderPitch")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "RShoulderRoll")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "RElbowYaw")
+        {
+           buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "RElbowRoll")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "RWristYaw")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+        if (joint_state->name[i] == "RHand")
+        {
+            buffer.data.push_back(joint_state->position[i]);
+            // cout << joint_state->name[i] << endl;
+        }
+
+    }
+
+    // write data into array
+    idx = 0;
+    for(vector<float>::const_iterator iter = buffer.data.begin(); iter != buffer.data.end(); ++iter)
+    {
+        // store into temporary target motor state buffer
+        motor_r_arm_in[idx] = *iter;
+        idx++;
+    }
+
+    // display data on terminal
+    // cout << "Right arm joints:  ";
+    // for (int i=0; i<R_ARM_DOF; i++)// received motor state of the HEAD
+double motor_head_in[HEAD_DOF];
+    //     cout << motor_r_arm_in[i] << " ";
+    // cout << endl;
+
+}
 
 
 /***************************
@@ -86,6 +281,12 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "moveleg_node");
     ros::NodeHandle ml_node_nh;
+
+    // subscribe to joint states
+    joint_state_sub = ml_node_nh.subscribe("joint_states", 1, &jointStateCB);
+
+    // advertise the target joint states
+    target_joint_state_pub = ml_node_nh.advertise<robot_specific_msgs::JointAnglesWithSpeed>("joint_angles", 1);
 
     // advertise joint stiffnesses
     stiffness_pub = ml_node_nh.advertise<robot_specific_msgs::JointState>("joint_stiffness", 1);
