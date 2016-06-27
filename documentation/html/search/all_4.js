51c910b51e118356be5a4157cb210b71feb5609e
Emilka <emilka.skurzynska@gmail.com>
2016-06-27 21:34:39 +0200

    Reward function, transitions, N and Q almost ready. Q needs improvement

--- documentation/html/search/all_4.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['set_5falpha',['set_alpha',['../class_c_m_a_c.html#a03913bba8d3a52c6b515cb235dcb62f5',1,'CMAC']]],
-  ['set_5fmax_5ftrain_5fiterations',['set_max_train_iterations',['../class_c_m_a_c.html#a1e84296568bd4575f770a8394d1022a9',1,'CMAC']]]
-];
